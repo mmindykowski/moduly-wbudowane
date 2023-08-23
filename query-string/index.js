f0248d34635d88aae7ccfe0e595415fb7da90bcb
mmindykowski <128606230+mmindykowski@users.noreply.github.com>
2023-08-23 19:58:32 +0200

    przetworzonie na ciag znakow

--- query-string/index.js
+++ query-string/index.js
@@ -1,4 +1,15 @@
-const querystring = require('querystring');
-const band = querystring.parse('name=beatles&members=john&members=paul&members=george&members=ringo');
- 
+const querystring = require("querystring");
+const band = querystring.parse(
+  "name=beatles&members=john&members=paul&members=george&members=ringo"
+);
+
 console.log(band);
+
+let user = {
+  name: "john",
+  password: "qwerty",
+};
+
+let stringFromObj = querystring.stringify(user);
+
+console.log(stringFromObj);
